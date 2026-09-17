eec2824b25910726ce2a9df96271a8981bf1972a
Sanchit990 <sanchitchauhan40@gmail.com>
2026-09-17 23:52:10 +0530

    Solved LCA in BST

---
+++ BinarySearchTrees/lcainbst.java
@@ -0,0 +1,29 @@
+package BinarySearchTrees;
+
+import javax.swing.tree.TreeNode;
+
+public class lcainbst {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode left=null;
+        TreeNode right=null;
+        if(root==null||root==p||root==q){
+            return root;
+        }
+        if(root.val>p.val||root.val>q.val)
+        left=lowestCommonAncestor(root.left,p,q);
+        if(root.val<p.val||root.val<q.val)
+        right= lowestCommonAncestor(root.right,p,q);
+        if(right!=null&&left!=null){
+            return root;
+        }
+        if(right==null&&left!=null){
+            return left;
+        }
+        if(right!=null&&left==null){
+            return right;
+        }
+        else
+            return null;
+    }
+}
+
