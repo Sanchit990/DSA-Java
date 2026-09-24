2a2be3523ebccc1acd08dfc331bfc8e688a2c249
Sanchit990 <sanchitchauhan40@gmail.com>
2026-09-24 22:43:10 +0530

    Solved Fibonacci Number Using DP

---
+++ DP/FibonacciNumber.java
@@ -0,0 +1,17 @@
+package DP;
+public class FibonacciNumber {
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        int prev2=0;
+        int prev=1;
+        int cur=0;
+      for(int i=2;i<=n;i++){
+        cur=prev+prev2;
+        prev2=prev;
+        prev=cur;
+      } 
+      return prev;
+    }
+}
